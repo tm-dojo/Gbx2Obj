71455a9530013ea2c574a062505ebc6a820e5340
Bux42 <roszwoka2@gmail.com>
2023-02-19 12:43:18 +0100

    init repo

---
+++ Gbx2Obj/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Please input the absolute path of the Trackmania\\Items folder:");
+string blocksPath = Console.ReadLine();
+
+if (!Directory.Exists(blocksPath))
+{
+    Console.WriteLine($"The folder {blocksPath} does not exist");
+    return;
+}
+
+getAllFoldersWithGbxFiles(blocksPath);
+
+List<string> getAllFoldersWithGbxFiles(string blocksPath)
+{
+    List<string> folders = Directory.EnumerateDirectories(blocksPath).ToList();
+
+    foreach (string folder in folders)
+    {
+        Console.WriteLine(folder);
+    }
+
+    return folders;
+}
