a96913cef2a7216ff5e0ed87086954202b221348
Bux42 <roszwoka2@gmail.com>
2023-09-18 19:28:23 +0200

    Implement basic clips extractor and Gbx to Obj converter

---
+++ Gbx2Obj/ClipsExtractor.cs
@@ -0,0 +1,93 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using GBX.NET.Engines.GameData;
+using GBX.NET.Engines.Plug;
+using GBX.NET.Inputs;
+using TmEssentials;
+using static GBX.NET.Engines.Game.CGameCtnGhost;
+
+using GBX.NET;
+using GBX.NET.BlockInfo;
+using GBX.NET.Engines.Game;
+using GBX.NET.Exceptions;
+using System;
+using System.Linq;
+using System.Text.Json;
+
+Console.WriteLine("Please input the absolute path of the .pak folder:");
+
+string blockFolder = Console.ReadLine();
+
+
+
+GameBox.OpenPlanetHookExtractMode = true;
+
+//var block2 = GameBox.ParseNode<CGameCtnBlockInfoClassic>(@"C:\Users\Bux\OpenplanetNext\Extract\GameData\Stadium\GameCtnBlockInfo\Sunrise\RoadTechChicaneX2Left.EDClassic.Gbx");
+
+
+var dict = new Dictionary<string, BlockModel>();
+
+foreach (var blockInfoFile in Directory.EnumerateFiles(blockFolder, "*.*", SearchOption.AllDirectories))
+{
+    try
+    {
+        var xd = GameBox.ParseNode(blockInfoFile);
+        if (GameBox.ParseNode(blockInfoFile) is not CGameCtnBlockInfoClassic blockInfo)
+        {
+            continue;
+        }
+
+        var block = new BlockModel
+        {
+            Air = CreateVariant(blockInfo.VariantBaseAir),
+            Ground = CreateVariant(blockInfo.VariantBaseGround)
+        };
+
+        dict.Add(blockInfo.Ident.Id, block);
+    }
+    catch (NotAGbxException)
+    {
+
+    }
+}
+
+Console.ReadLine();
+
+File.WriteAllText("Clips.json", JsonSerializer.Serialize(dict, new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull }));
+
+BlockInfoWriter.Write("Clips.bi", dict);
+var nice = BlockInfoReader.Read("Clips.bi");
+BlockInfoWriter.Write("Clips.bi", nice);
+var doubleNice = BlockInfoReader.Read("Clips.bi");
+
+Console.WriteLine();
+
+static BlockUnit[]? CreateVariant(CGameCtnBlockInfoVariant? blockInfoVariant)
+{
+    if (blockInfoVariant?.BlockUnitModels is null)
+    {
+        return null;
+    }
+
+    return blockInfoVariant
+        .BlockUnitModels
+        .OfType<CGameCtnBlockUnitInfo>()
+        .Select(x => new BlockUnit
+        {
+            Coord = x.RelativeOffset,
+            NorthClips = GetClipInfo(x.ClipsNorth),
+            EastClips = GetClipInfo(x.ClipsEast),
+            SouthClips = GetClipInfo(x.ClipsSouth),
+            WestClips = GetClipInfo(x.ClipsWest),
+            TopClips = GetClipInfo(x.ClipsTop),
+            BottomClips = GetClipInfo(x.ClipsBottom)
+        }).ToArray();
+}
+
+static string[]? GetClipInfo(ExternalNode<CGameCtnBlockInfoClip>[]? clips)
+{
+    return clips?.Length > 0
+        ? clips.Select(x => Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(x.File?.FileName)))
+        .OfType<string>()
+        .ToArray() : null;
+}
--- Gbx2Obj/Program.cs
+++ Gbx2Obj/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Please input the absolute path of the Trackmania\\Items folder:");
+﻿using GBX.NET;
+using GBX.NET.Engines.GameData;
+using GBX.NET.Engines.Plug;
+
+
+Console.WriteLine("Please input the absolute path of the Trackmania\\Items folder:");
 string blocksPath = Console.ReadLine();
 
 if (!Directory.Exists(blocksPath))
@@ -7,16 +12,141 @@ if (!Directory.Exists(blocksPath))
     return;
 }
 
-getAllFoldersWithGbxFiles(blocksPath);
+createLocalFolders(blocksPath);
+convertGbxToObj(blocksPath);
 
-List<string> getAllFoldersWithGbxFiles(string blocksPath)
+string removeBlockExtension(string blockPath)
 {
+    return blockPath.Replace(".Item.Gbx", "");
+}
+
+string getLocalPathFromItemFolder(string itemFolderPath)
+{
+    // Convert the absolution path of the item folder (from the game's directory) to local path (Gbx2Obj/bin/Debug/net7.0/)
+    string localFolder = itemFolderPath.Replace(blocksPath, "");
+
+    if (localFolder.StartsWith("/") || localFolder.StartsWith("\\"))
+    {
+        localFolder = localFolder.Substring(1);
+    }
+    localFolder = "./" + localFolder;
+    return localFolder;
+}
+
+void createLocalFolders(string blocksPath)
+{
+    // Browse through the game Items folders recursively, create same arborescence locally (Gbx2Obj/bin/Debug/net7.0/)
+
     List<string> folders = Directory.EnumerateDirectories(blocksPath).ToList();
 
     foreach (string folder in folders)
     {
-        Console.WriteLine(folder);
+        string localFolder = getLocalPathFromItemFolder(folder);
+        if (!Directory.Exists(localFolder))
+        {
+            Console.WriteLine($"Creating local folder {localFolder}");
+            Directory.CreateDirectory(localFolder);
+        }
+        createLocalFolders(folder);
+    }
+}
+
+void ExportCrystalObj(CPlugCrystal crystal, string filePath)
+{
+    FileStream objStream = new FileStream(filePath + ".obj", FileMode.OpenOrCreate);
+    FileStream mtlStream = new FileStream(filePath + ".mtl", FileMode.OpenOrCreate);
+
+    crystal.ExportToObj(objStream, mtlStream);
+}
+void getBlockMesh(string blockPath)
+{
+    string outputObjPath = getLocalPathFromItemFolder(blockPath);
+    outputObjPath = removeBlockExtension(outputObjPath);
+
+    Console.WriteLine($"outputObjPath {outputObjPath}");
+
+    try
+    {
+        GameBox<CGameItemModel> gbx = GameBox.Parse<CGameItemModel>(blockPath);
+
+        if (gbx.Node is CGameItemModel)
+        {
+            CGameItemModel item = gbx.Node;
+
+            if (item.BlockModel != null)
+            {
+                foreach (KeyValuePair<int, CPlugCrystal> variant in item.BlockModel.CustomizedVariants)
+                {
+                    CPlugCrystal crystal = variant.Value;
+
+                    if (crystal != null)
+                    {
+                        ExportCrystalObj(crystal, outputObjPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("File with BlockModel (variant " + variant.Key + ") has no Crystal: \t" + outputObjPath);
+                    }
+                }
+            }
+            else if (item.ItemModel != null)
+            {
+                CPlugCrystal crystal = item.ItemModel.MeshCrystal;
+
+                if (crystal != null)
+                {
+                    ExportCrystalObj(crystal, outputObjPath);
+                }
+                else
+                {
+                    Console.WriteLine("File with ItemModel contained no Crystal: " + blockPath);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File contained no BlockModel or ItemModel: " + blockPath);
+            }
+        }
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine($"Error converting {blockPath}: {e}");
+    }
+}
+
+void convertGbxToObj(string blocksPath)
+{
+    // Browse through the game Items folders recursively, convert each .Item.Gbx file to .obj file (output is at Gbx2Obj/bin/Debug/net7.0/)
+    List<string> folderFiles = Directory.EnumerateFiles(blocksPath).ToList();
+
+    if (folderFiles.Count > 0 &&
+        (folderFiles.Any(file => file.EndsWith(".Item.Gbx")) || folderFiles.Any(file => file.EndsWith(".Solid.Gbx")))
+        )
+    {
+        List<string> gbxFiles = folderFiles.FindAll(file => file.EndsWith(".Item.Gbx") || file.EndsWith(".Solid.Gbx"));
+        Console.WriteLine($"Found {gbxFiles.Count} files to convert in {blocksPath}");
+
+        foreach (string file in gbxFiles)
+        {
+            //if (!file.Contains("RoadTechDiagRightStartCurve1In"))
+            //    continue;
+            string localFilePath = getLocalPathFromItemFolder(file);
+            if (!File.Exists(localFilePath))
+            {
+                string[] fileSplit = file.Split("\\");
+                string fileName = fileSplit[fileSplit.Length - 1];
+                Console.WriteLine($"Convert {removeBlockExtension(fileName)}");
+                getBlockMesh(file);
+            }
+        }
     }
 
-    return folders;
+    List<string> folderSubFolders = Directory.EnumerateDirectories(blocksPath).ToList();
+    if (folderSubFolders.Count > 0)
+    {
+        foreach (string subFolder in folderSubFolders)
+        {
+            convertGbxToObj(subFolder);
+        }
+    }
 }
